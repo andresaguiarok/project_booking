5e9eaf09917ca8153352234f229c9a5e5f94df6e
andresaguiarok <andresaguiarok@outlook.com>
2024-03-10 15:00:52 -0300

    importamos el endpoint de api/users

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const booking = require('./routers/bookings.router.js')
-const connection = require('./config/connection.js')
-require('dotenv').config()
+const express = require('express');
+const booking = require('./routers/bookings.router.js');
+const users   = require('./routers/users.router.js');
+const connection = require('./config/connection.js');
+require('dotenv').config();
 
-const app   = express()
-const port  = process.env.PORT
+const app   = express();
+const port  = process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/api/bookings', booking)
+app.use('/api/bookings', booking);
+app.use('/api/users', users);
 
-connection.ConnectionDB()
+connection.ConnectionDB();
 
 app.listen(port,() => {
-    console.log(`running on port: ${port}`)
-})
+    console.log(`running on port: ${port}`);
+});
