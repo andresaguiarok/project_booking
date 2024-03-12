fe4a7778192887dbeea4812b072bb9a268580add
andresaguiarok <andresaguiarok@outlook.com>
2024-03-12 20:53:00 -0300

    desarrollo del user dao

---
+++ src/daos/mongo_db/user.mongo.js
@@ -0,0 +1,14 @@
+const userModel = require('../models/user.model.js');
+
+class UserDao {
+    async get(){};
+    async getID(){};
+    async post(){};
+    async update(){};
+    async deleteID(){};
+    async delete(){};
+};
+
+module.exports = {
+    UserDao
+};
