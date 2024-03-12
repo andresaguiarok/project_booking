06165e5c232ff21d105c5959aef6a0f6d6a47bee
andresaguiarok <andresaguiarok@outlook.com>
2024-03-12 20:50:16 -0300

    desarrollo del modelo de usuarios

---
+++ src/daos/models/user.model.js
@@ -0,0 +1,36 @@
+const {Schema, Model} = require('mongoose');
+const collection = 'user';
+
+const userSchema = new Schema({
+    firts_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    birthdate:{
+        type: Date,
+    },
+    role: {
+        type: String,
+        default: 'user'
+    }
+
+});
+
+const userModel = Model(collection, userSchema);
+
+module.exports = {
+    userModel
+};
