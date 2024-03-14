5d4b82d8220428efbff7f2da909e908e56230ddc
andresaguiarok <andresaguiarok@outlook.com>
2024-03-14 20:46:39 -0300

    desarrollo de los metodos: get y getID

--- src/daos/mongo_db/user.mongo.js
+++ src/daos/mongo_db/user.mongo.js
@@ -1,8 +1,24 @@
-const userModel = require('../models/user.model.js');
+const { userModel } = require('../models/user.model.js');
 
 class UserDao {
-    async get(){};
-    async getID(){};
+
+    async get(){
+        try {
+            return await userModel.find();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    async getID(data){
+        try {
+            console.log(data);
+            return await userModel.findOne(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     async post(){};
     async update(){};
     async deleteID(){};
